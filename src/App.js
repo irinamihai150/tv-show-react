774c592c11286e3ae2405e77c2c9d37d6858cb4d
irina mihai <irinamihai150@gmail.com>
2022-12-15 20:21:48 +0000

    fetching shows

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+// function ShowCard() {
+//   const [shows, setShow] = useState();
+//   useEffect(() => {
+//     fetch(`https://api.tvmaze.com/shows/82/episodes`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setShow(data);
+//         console.log(shows);
+//       });
+//   }, [shows]);
+//   shows.map((show) => {
+//     return <h1> {show.name}</h1>;
+//   });
+// }
 
 function App() {
+  const [data, setData] = useState();
+
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
+  useEffect(() => {
+    fetch(`https://api.tvmaze.com/shows/82/episodes`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data : ", error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  if (loading) return "Loading....";
+  if (error) return "Error!!!";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Tv show</h2>
+      <div class="mx-20">
+        <ul>
+          {data.map((show) => (
+            <div class="">
+              <p>{show.name}</p>
+            </div>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
